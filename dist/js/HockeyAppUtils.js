be51cd511bea31e1f26febf778d4fbc2acf55848
Miguel Ángel Domínguez Coloma <miguel.dominguez@betssongroup.com>
2016-08-23 14:54:36 +0200

    to lower case match apps

--- dist/js/HockeyAppUtils.js
+++ dist/js/HockeyAppUtils.js
@@ -13,7 +13,7 @@ var HockeyAppUtils = (function () {
     HockeyAppUtils.getAppByTitleMatch = function (appsResponse, match) {
         var apps = appsResponse.apps;
         for (var i = 0; i < apps.length; i++) {
-            if (apps[i].title == match) {
+            if (apps[i].title.toLowerCase() == match.toLowerCase()) {
                 return apps[i];
             }
         }
